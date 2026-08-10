5d0aa25728c2a7cd7c0140fe735e8063a621daac
Adam Simon <adamosimoni@gmail.com>
2026-08-10 16:26:03 +0200

    Update samples

--- samples/next-js-sample/next.config.mjs
+++ samples/next-js-sample/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // You may need the following setting in newer Next.js versions if you
+  // get the error "Cannot read properties of null (reading 'useContext')".
+  transpilePackages: ["configcat-react"],
+  turbopack: {
+    root: "."
+  }
 };
 
 export default nextConfig;
